df35e3212dcb6b0b4d31ee83d6c13902ee9241ef
Brian Hinton <briannhinton@gmail.com>
2017-10-20 20:05:36 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,35 @@ clear
 
 echo "wget git coreutils nodejs golang zsh ruby vim neo-vim python and make installed successfully!"
 
+# make a new dotfiles directory
+mkdir ~/dotfiles 
+
+# enter the new dotfiles directory
+cd ~/dotfiles 
+
+# ready the directory for git version control
+git init 
+
+# creating the config files
+touch zshrc tmux.conf vimrc 
+
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 # Instal 
 pip3 install neovim
 
+# Remove created folders
+rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2> /dev/null
+
+# Make required folders for neo-vim
+mkdir -p ~/.config ~/.config/nvim
+
+# Symlink the files
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
+
+# Check to see if a .termux folder exists. If it does create a back-up
 if [ -d "$HOME/.termux" ]; then
  mv $HOME/.termux $HOME/.termux.bak
 fi
@@ -22,7 +46,6 @@ curl -fsLo $HOME/.termux/colors.properties --create-dirs https://raw.githubuserc
 
 # Install font
 curl -fsLo $HOME/.termux/font.ttf --create-dirs https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-Retina.ttf
-
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
