fd7bf28559b8c54d7bc68866a463acd06ff9a7e7
Brian Hinton <briannhinton@gmail.com>
2017-10-20 20:34:46 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ here=$(pwd)
 apt update && apt upgrade
 apt remove wget
 
-apt install -y wget git coreutils nodejs golang zsh gcc ruby python python-dev make vim neovim
+apt install -y wget git coreutils nodejs 
+apt install -y golang zsh gcc ruby python
+apt install -y python-dev make vim neovim
 clear
 
 echo "wget git coreutils nodejs golang zsh gcc ruby vim neo-vim python and make installed successfully!"
